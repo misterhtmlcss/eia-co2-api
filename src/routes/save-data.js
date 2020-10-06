a7f6142fdf4689ca9bb2bd89cfdc53cdaedb8c70
Roger Kondrat <misterhtmlcss@gmail.com>
2020-10-05 21:47:18 -0600

    started building out data download api

---
+++ src/routes/save-data.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+// Route
+// http://localhost:5000/save/test?states=alabama
+const saveData = (
+  API_KEY,
+  getData
+) => {
+  return router.get("/data", async (req, res, next) => {
+    try {
+      const { state } = req.query;
+      const { codex, findStateCode } = res.locals
+      const label = findStateCode(state, codex)
+
+      const url = `https://api.eia.gov/series/?api_key=${API_KEY}&series_id=EMISS.CO2-TOTV-EC-CO-${label}`;
+
+
+      const { data } = await getData("CO2", url, axios);
+      console.log("data", data);
+
+
+      return res.status(200).json({state, data});
+    } catch (err) {
+      next(err)
+      // return res.status(500).json({ message: "Error!!!" });
+    }
+  });
+};
+
+module.exports = saveData;
